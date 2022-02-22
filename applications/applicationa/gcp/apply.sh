84e1e446778146ea86327bb50a06fd73b2d3a3d8
evekhm <evekhm@gmail.com>
2022-02-22 00:25:39 -0800

    service issues

--- applications/applicationa/gcp/apply.sh
+++ applications/applicationa/gcp/apply.sh
@@ -6,11 +6,13 @@ PWD=$(pwd)
 cd "$GCP"/../k8s/ || exit
 
 echo "***** Applying  $APPLICATION deployment  *****"
-kubectl apply -f service.yaml --namespace="$KUBE_NAMESPACE"
+
 
 sed 's|__IMAGE_TAG__|'"$IMAGE"'|g;' deployment.sample.yaml > deployment.yaml
 kubectl apply -f deployment.yaml --namespace="$KUBE_NAMESPACE"
 
+kubectl get svc applicationa -n "$KUBE_NAMESPACE" 2>/dev/null || kubectl expose deployment applicationa --port=5001 --target-port=80 -n "$KUBE_NAMESPACE" --type=LoadBalancer
+#kubectl apply -f service.yaml --namespace="$KUBE_NAMESPACE" TODO
 
 cd "$PWD" || exit
 
