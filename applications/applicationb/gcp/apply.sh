99a7f6caf42d9187a7565dc20578732b9b0ad93e
evekhm <evekhm@gmail.com>
2022-03-07 22:09:34 -0800

    Working on deploy trigger for CloudStorage Upload

--- applications/applicationb/gcp/apply.sh
+++ applications/applicationb/gcp/apply.sh
@@ -2,7 +2,7 @@
 set -e # Exit if error is detected during pipeline execution
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-APPLICATION=$(basename "${DIR}")
+APPLICATION=$(basename "${DIR}../")
 PROJECT_PATH="${APPLICATION_NAMESPACE}/${APPLICATION}"
 
 DB="$APPLICATION".zip
